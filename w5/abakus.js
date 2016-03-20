6f62deda4b68a6e48f2762f40198bc1a9c333714
mihkel <mihkelmatson@gmail.com>
2016-03-20 15:08:52 +0200

    w5 Lisatud abakus.js

--- w5/abakus.js
+++ w5/abakus.js
@@ -1,13 +1,22 @@
 window.onload = function() {
 	// lehe laadimine lõpetatud. Siia funktsiooni sisse kirjutan elementide mõjutamise käsud
 	var beads = document.getElementsByClassName('bead');
-	console.log(beads);
-
-	for (var i = beads.length - 1; i >= 0; i--) {
-		var vaartus = beads[i].style.cssFloat;
-		beads[i].style.backgroundColor = "red";
-		console.log(i, vaartus);
+	for (var i = 0; i < beads.length; i++) {
+		beads[i].onclick = function(a) {
+			if (this.getAttribute('style') == null || this.getAttribute('style') == "") {
+				this.style.cssFloat = "right";
+			}
+			else {
+				if (this.style.cssFloat == "left") {
+					this.style.cssFloat = "right";
+				}
+				else {
+					this.style.cssFloat = "left";
+				}
+			}
+			
+		};
 	};
-
+	
 
 };
